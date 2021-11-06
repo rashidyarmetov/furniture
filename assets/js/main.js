3261bd777f2356cdf2db627d56da980be3f6ed49
rashidyarmetov <91193293+rashidyarmetov@users.noreply.github.com>
2021-11-06 11:32:36 +0400

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('.slider_number').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true
+    });
+
+    $('.advantages_slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    });
+});
